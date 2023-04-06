f46cae61378809bc74fa1fb222ba7dc533bda8a7
Atlanswer <atlanswer@gmail.com>
2023-04-06 18:02:55 +0800

    solidjs template

---
+++ README.md
@@ -0,0 +1,24 @@
+# Solid Template
+
+Template for SolidJS, configured with
+
+- TypeScript
+- Prettier
+- ESLint
+- Tailwind CSS
+
+VSCode settings are included for convenience.
+
+## Usage
+
+```shell
+# Development
+pnpm i
+pnpm dev
+# Lint and fix
+pnpm lint
+pnpm lint --fix
+# build and preview
+pnpm build
+pnpm serve
+```
---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import type { Component } from "solid-js";
+
+const App: Component = () => {
+  return (
+    <main class="flex min-h-screen w-screen flex-col place-content-center place-items-center gap-4">
+      <p class="bg-gradient-to-tr from-blue-700 to-blue-400 bg-clip-text text-center align-middle font-sans text-6xl font-bold leading-normal text-transparent">
+        Solid Template
+      </p>
+    </main>
+  );
+};
+
+export default App;
